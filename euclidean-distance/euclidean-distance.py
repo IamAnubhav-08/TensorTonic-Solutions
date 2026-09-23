b8ef77b397d6658d9f44c9996ff228a9b5c340fd
Anubhav Santra <75220234+IamAnubhav-08@users.noreply.github.com>
2026-09-23 20:51:05 +0530

    Solve euclidean-distance

--- euclidean-distance/euclidean-distance.py
+++ euclidean-distance/euclidean-distance.py
@@ -5,8 +5,5 @@ def euclidean_distance(x: list, y: list) -> float:
     Returns the Euclidean distance as a Python float.
     """
     # Write code here
-    sum = 0
-    for i in range(0, len(x)):
-        d = abs(x[i]-y[i])**2
-        sum = sum + d
-    return np.sqrt(sum)
+    z = np.array(x)-np.array(y)
+    return np.linalg.norm(z)
